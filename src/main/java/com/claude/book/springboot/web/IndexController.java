6591bad15ec36358828200ae56a3af94e2a997f9
claudekim52 <javadeveloper@kakao.com>
2020-06-23 20:55:07 +0900

    m1

--- src/main/java/com/claude/book/springboot/web/IndexController.java
+++ src/main/java/com/claude/book/springboot/web/IndexController.java
@@ -1,35 +1,17 @@
 package com.claude.book.springboot.web;
 
-import com.claude.book.springboot.service.posts.PostsService;
-import com.claude.book.springboot.web.dto.PostsResponseDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-@RequiredArgsConstructor
 @Controller
 public class IndexController {
-
-    private final PostsService postsService;
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index() {
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "postssave";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post",dto);
-
-        return "postsupdate";
     }
 }
